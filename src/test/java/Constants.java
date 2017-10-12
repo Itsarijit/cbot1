c4de9f19c654fb0e6beea3cf553c7590e57a2b53
Itsarijit <itsarijitpaul@gmail.com>
2017-10-12 15:46:55 +0200

    pass

--- src/test/java/Constants.java
+++ src/test/java/Constants.java
@@ -2,5 +2,5 @@
 public class Constants {
 private Constants() {}
    public static final String user_name = "itsarijitpaul@gmail.com";
-   public static final String user_Password = "QW1pQHBpa2x1JDExMQ";
+   public static final String user_Password = "";
 }
